dfd0942047d060a1f716cde447611112c68501cb
Ramsundhar Venkadesan <42778998+ramsundharVenkadesan@users.noreply.github.com>
2026-04-14 17:36:31 -0400

    Add files via upload

---
+++ CliniGraph/Main.tf
@@ -0,0 +1,32 @@
+locals {
+  services = [
+    "compute.googleapis.com",
+    "storage.googleapis.com",
+    "cloudkms.googleapis.com",
+    "run.googleapis.com"
+  ]
+}
+
+resource "google_project_service" "enable_apis" {
+  for_each = toset(local.services)
+  project  = "cliniclarity"
+  service  = each.value
+}
+
+module "storage" {
+  source     = "./Storage"
+  depends_on = [google_project_service.enable_apis]
+}
+
+
+module "compute" {
+  source     = "./Compute"
+  depends_on = [google_project_service.enable_apis]
+
+  google_api_key    = var.google_api_key
+  pinecone_api_key  = var.pinecone_api_key
+  huggingface_token = var.hugging_face_token
+  index             = pinecone_index.serverless.name
+  langchain_api_key = var.langchain_api_key
+  cache_bucket_name = module.storage.storage_bucket
+}
