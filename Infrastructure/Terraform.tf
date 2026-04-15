b81b5fef99fbc58c57b63bf1f9d7bde94670c93a
Ramsundhar Venkadesan <42778998+ramsundharVenkadesan@users.noreply.github.com>
2026-04-14 20:18:27 -0400

    Update Terraform.tf

--- Infrastructure/Terraform.tf
+++ Infrastructure/Terraform.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = "~>1.14.0"
 
+  backend "gcs" {
+    bucket = "cliniclarity-tf-state"
+    prefix  = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
