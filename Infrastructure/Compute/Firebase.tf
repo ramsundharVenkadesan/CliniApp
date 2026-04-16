6fec82dbf71d605e78017f779ac88fc684e1fb14
Ramsundhar Venkadesan <42778998+ramsundharVenkadesan@users.noreply.github.com>
2026-04-16 09:17:59 -0400

    Update Firebase.tf

--- Infrastructure/Compute/Firebase.tf
+++ Infrastructure/Compute/Firebase.tf
@@ -1,7 +1,4 @@
-import {
-  to = module.compute.google_identity_platform_config.auth_config
-  id = "projects/cliniclarity"
-}
+
 
 resource "google_firebase_project" "cliniclarity_firebase" {
   provider = google-beta
